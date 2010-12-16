6a77203e9e2341be888d430acc881afff51ed312
Leigh Caplan <lcaplan@onehub.com>
2010-12-16 14:11:37 -0800

    Disable default order for now. This might break some esoteric features, but it clobbers all other order scopes

--- lib/awesome_nested_set.rb
+++ lib/awesome_nested_set.rb
@@ -63,7 +63,7 @@ module CollectiveIdea #:nodoc:
             belongs_to :parent, :class_name => self.base_class.to_s,
               :foreign_key => parent_column_name
             has_many :children, :class_name => self.base_class.to_s,
-              :foreign_key => parent_column_name, :order => quoted_left_column_name
+              :foreign_key => parent_column_name #, :order => quoted_left_column_name
 
             attr_accessor :skip_before_destroy
           
